cc19dd4ac884443d4a09ac11c946058ec86737c2
vov4uk95 <vov4uk9505@gmail.com>
2025-04-14 07:47:44 +0300

    Create products.js

---
+++ utils/products.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: '1',
+    name: 'Елегантен комплект',
+    price: 89,
+    category: 'комплекти',
+    image: 'https://via.placeholder.com/200x250?text=Комплект',
+  },
+  {
+    id: '2',
+    name: 'Черни клин',
+    price: 49,
+    category: 'панталони и клинове',
+    image: 'https://via.placeholder.com/200x250?text=Клин',
+  },
+  {
+    id: '3',
+    name: 'Лятна рокля',
+    price: 99,
+    category: 'рокли',
+    image: 'https://via.placeholder.com/200x250?text=Рокля',
+  },
+  {
+    id: '4',
+    name: 'Бяла блуза',
+    price: 59,
+    category: 'ризи и блузи',
+    image: 'https://via.placeholder.com/200x250?text=Блуза',
+  }
+];
